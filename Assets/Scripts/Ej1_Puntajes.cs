322585bc94135fa3eabf1dfa7a9a21542ea39ad2
LuchoDelArco <47138072@est.ort.edu.ar>
2022-08-19 10:49:20 -0300

    ClassFinished

---
+++ Assets/Scripts/Ej1_Puntajes.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Para almacenar el puntaje obtenido en un juego que tiene 10 desafíos crea un array de 10 posiciones donde guardaremos 
+//cuántos puntos se obtuvieron en cada desafío.Implementa las siguientes funciones:
+//a. int ObtenerMayorPuntaje()
+//Devuelve el puntaje más alto de los 10 almacenados
+//b. int ObtenerNivelDeMenorPuntaje()
+//Devuelve el índice que tiene el menor puntaje (si hay más de un nivel con puntaje mínimo idéntico, devolver el primero encontrado)
+//c. float ObtenerPuntajePromedio()
+
+
+
+
+public class Ej1_Puntajes : MonoBehaviour
+{
+	[SerializeField] int[] puntajes = new int[10];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("El mayor puntaje es: " + ObtenerMayorPuntaje());
+		Debug.Log("El nivel de menor puntaje es: " + ObtenerNivelDeMenorPuntaje());
+		Debug.Log("El puntaje promedio es: " + ObtenerPuntajePromedio());
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	int ObtenerMayorPuntaje()
+	{
+		int mayorPuntaje = 0;
+
+		foreach (int puntaje in puntajes)
+		{
+			if ( puntaje > mayorPuntaje)
+			{
+				mayorPuntaje = puntaje;
+			}
+		}
+
+		return mayorPuntaje;
+	}
+
+	int ObtenerNivelDeMenorPuntaje()
+	{
+		int indiceNivelMenorPuntaje = 0;
+		int menorPuntaje = puntajes[0];
+
+		for(int i= 1; i < puntajes.Length; i++)
+		{
+			if (puntajes[i] < menorPuntaje)
+			{
+				menorPuntaje = puntajes[i];
+				indiceNivelMenorPuntaje = i;
+			}
+		}
+
+		return indiceNivelMenorPuntaje;
+	}
+
+	float ObtenerPuntajePromedio()
+	{
+		float puntajePromedio = 0;
+		float suma = 0;
+
+		foreach (int num in puntajes)
+		{
+			suma += num;
+		}
+		puntajePromedio = suma / puntajes.Length;
+
+		return puntajePromedio;
+	}
+
+}
---
+++ Assets/Scripts/Ej2_Dias_Y_Meses.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Creá un array de Strings que contenga los nombres de cada uno de los doce meses del año, y otro que contenga la cantidad de días de cada uno(ignorá
+//los años bisiestos). Luego implementá las siguientes funciones
+//a. bool ValidarFormatoFecha(string fecha) Devuelve verdadero si la string recibida representa una fecha válida. La regla es que sea un
+//string de 4 caracters donde los dos primeros caracteres sean un número entre 01 y 31 y los dos últimos un número entre 01 y 12.
+//b.  public String getNombreDelMes(int numeroMes) Recibe el número de mes(entre 1 y 12) y devuelve el nombre del mes en cuestión.
+//c.  public int diasTranscurridos(int numeroMes) Recibe el número de mes y devuelve la cantidad de días transcurridos en el año antes de 
+//comenzar el mes en cuestión.
+//Al presionar los botones correspondientes mostrá cuántos días transcurrieron desde el comienzo del año y qué día del año (cuántos días pasaron hasta ese día) es un día determinado ingresado en formato ddmm.
+
+
+
+public class Ej2_Dias_Y_Meses : MonoBehaviour
+{
+
+	[SerializeField] string[] meses = new string[12];
+	[SerializeField] int[] dias = new int[12];
+	public string fechaIngresada;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log(ValidarFormatoFecha(fechaIngresada));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	bool ValidarFormatoFecha(string fecha)
+	{
+		bool fechaValida = false;
+
+		if (fecha.Length == 4)
+		{
+			string dia = "";
+			string mes = "";
+
+			mes += fecha[2];
+			mes += fecha[3];
+
+			dia += fecha[0];
+			dia += fecha[1];
+
+			int diaNumber = int.Parse(dia);
+			int mesNumber = int.Parse(mes);
+
+			Debug.Log("Dias: " + diaNumber);
+			Debug.Log("Mes: " + mesNumber);
+
+			if (diaNumber > 0 && diaNumber <= 31 && mesNumber > 0 && mesNumber <= 12)
+			{
+				fechaValida = true;
+			}
+
+		}
+		else
+		{
+			Debug.Log("El largo de la fecha es incorrecto");
+		}
+
+		return fechaValida;
+	}
+}
